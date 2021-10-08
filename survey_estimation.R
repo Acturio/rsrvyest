e9cb4729b1eac738e8de6501fbe11d0a0d6bcd4a
Acturio <act.arturo.b@ciencias.unam.mx>
2021-10-08 00:42:22 -0500

    Se guardan  librerías

--- survey_estimation.R
+++ survey_estimation.R
@@ -1,69 +1,86 @@
 # PRUEBA PREGUNTAS NUMÉRICAS Y CATEGÓRICAS
 source("~/Desktop/UNAM/DIAO/rsrvyest/src/utils.R")
- 
+
+# library(srvyr)
+# library(magrittr)
+# library(reshape2)
+# library(foreign)
+# library(purrr)
+# library(ggplot2)
+# library(readxl)
+# library(survey)
+# library(dplyr)
+# library(tidyr)
+# library(xlsx)
+# library(caret)
+# library(openxlsx)
+# library(reshape)
+# library(tibble)
+# library(stringr)
+
  # Lectura de datos
 
  {
    # Lectura de datos de spss
-   dataset <- read.spss("data/BASE_CONACYT_260118.sav", to.data.frame = TRUE) 
+   dataset <- read.spss("data/BASE_CONACYT_260118.sav", to.data.frame = TRUE)
 
    # Nombre de estimación global (Puede ser nacional, cdmx, etc. Depende de la representatividad del estudio)
-   General <- "Nacional" 
-   
+   General <- "Nacional"
+
    Lista <- read_xlsx(
      path = "aux/Lista de Preguntas.xlsx",
      sheet = "Lista Preguntas"
-     ) %>% pull(Pregunta) 
-   
+     ) %>% pull(Pregunta)
+
    Lista_Preg <- read_xlsx(
-     path = "aux/Lista de Preguntas.xlsx", 
+     path = "aux/Lista de Preguntas.xlsx",
      sheet = "Lista Preguntas"
      ) %>% pull(Nombre)
-   
+
    DB_Mult <- read_xlsx(
-     path = "aux/Lista de Preguntas.xlsx", 
+     path = "aux/Lista de Preguntas.xlsx",
      sheet = "Múltiple"
      ) %>% as.data.frame()
-   
+
    Lista_Cont <- read_xlsx(
      path = "aux/Lista de Preguntas.xlsx",
      sheet = "Continuas"
      ) %>% path(VARIABLE)
-   
+
    Dominios <- read_xlsx(
      path = "aux/Lista de Preguntas.xlsx",
      sheet = "Dominios"
      ) %>% pull(Dominios)
-   
+
    Multiples <- names(DB_Mult)
    Ponderador <- pull(dataset, Pondi1)
    save = ""
  }
- 
+
 # Diseños
- 
+
 {
   disenio_cat <- disenio(
-    id = c(CV_ESC, ID_DIAO), 
-    estrato = ESTRATO, 
+    id = c(CV_ESC, ID_DIAO),
+    estrato = ESTRATO,
     pesos = Pondi1,
-    reps = FALSE, 
+    reps = FALSE,
     datos = dataset
     )
-  
+
   disenio_cont <- disenio(
-    id = CV_ESC, 
-    estrato = ESTRATO, 
+    id = CV_ESC,
+    estrato = ESTRATO,
     pesos = Pondi1,
-    reps = FALSE, 
+    reps = FALSE,
     datos = dataset
     )
-  
+
   disenio_mult <- disenio(
-    id = c(CV_ESC, ID_DIAO), 
-    estrato = ESTRATO, 
+    id = c(CV_ESC, ID_DIAO),
+    estrato = ESTRATO,
     pesos = Pondi1,
-    reps = FALSE, 
+    reps = FALSE,
     datos = dataset
     )
 }
@@ -73,7 +90,7 @@ source("~/Desktop/UNAM/DIAO/rsrvyest/src/utils.R")
   wb <- openxlsx::createWorkbook()
   options("openxlsx.numFmt" = "0.0")
 # Empezamos en el renglón 5
-  
+
   k1 <- 5
   k2 <- 5
   k3 <- 5
@@ -82,54 +99,54 @@ source("~/Desktop/UNAM/DIAO/rsrvyest/src/utils.R")
 
   organismo <- 'Ciudadanía mexicana'
   nombre_proyecto <- 'Conacyt 2018'
-  
+
   openxlsx::addWorksheet(wb, sheetName = 'Frecuencias simples')
   showGridLines(wb, sheet = 'Frecuencias simples', showGridLines = FALSE)
-  
+
   openxlsx::addWorksheet(wb, sheetName = 'Tablas cruzadas')
   showGridLines(wb, sheet='Tablas cruzadas', showGridLines = FALSE)
-  
+
   openxlsx::addWorksheet(wb, sheetName = 'Frecuencias (dispersión)')
   showGridLines(wb, sheet = 'Frecuencias (dispersión)', showGridLines = FALSE)
-  
+
   openxlsx::addWorksheet(wb, sheetName = 'Tablas cruzadas (dispersión)')
   showGridLines(wb, sheet = 'Tablas cruzadas (dispersión)', showGridLines = FALSE)
-  
+
 }
 
 # Estilos
 {
   headerStyle <- createStyle(
-    fontSize = 11, 
-    fontColour = "black", 
+    fontSize = 11,
+    fontColour = "black",
     halign = "center",
-    border = "TopBottom", 
+    border = "TopBottom",
     borderColour = "black",
-    borderStyle = c('thin', 'double'), 
+    borderStyle = c('thin', 'double'),
     textDecoration = 'bold'
     )
-  
+
   bodyStyle <- createStyle(
-    halign = 'center', 
+    halign = 'center',
     border = "TopBottomLeftRight",
-    borderColour = "black", 
+    borderColour = "black",
     borderStyle = 'thin',
-    valign = 'center', 
+    valign = 'center',
     wrapText = TRUE
     )
-  
+
   verticalStyle <- createStyle(
     border = "Right",
-    borderColour = "black", 
+    borderColour = "black",
     borderStyle = 'thin',
     valign = 'center'
     )
-  
+
   totalStyle <-  createStyle(numFmt = "###,###,###.0")
-  
+
   horizontalStyle <- createStyle(
     border = "bottom",
-    borderColour = "black", 
+    borderColour = "black",
     borderStyle = 'thin',
     valign = 'center'
     )
@@ -142,25 +159,25 @@ for (p in Lista[133:151]) {
 
   print(paste0("Estimando resultado de pregunta: ", p))
   dataset %<>% mutate(Pondi1 = Ponderador)
-  
+
   if (p %in% Multiples){
 
     print(paste('Estimación de pregunta múltiple:', p))
-    
+
     multiples <- preguntas(
-      pregunta = p, 
-      num_pregunta = np, 
+      pregunta = p,
+      num_pregunta = np,
       datos=dataset,
-      DB_Mult = DB_Mult, 
+      DB_Mult = DB_Mult,
       dominios = Dominios,
       lista_preguntas = Lista_Preg,
-      diseño = disenio_mult, 
-      wb = wb, 
+      diseño = disenio_mult,
+      wb = wb,
       renglon_fs = c(k1, k2),
-      renglon_tc = c(k3, k4), 
-      columna = 1, 
+      renglon_tc = c(k3, k4),
+      columna = 1,
       hojas_fs = c(1,3),
-      hojas_tc = c(2,4), 
+      hojas_tc = c(2,4),
       fuente = nombre_proyecto,
       tipo_pregunta = 'multiple',
       organismo_participacion = organismo,
@@ -168,39 +185,39 @@ for (p in Lista[133:151]) {
       estilo_categorias = bodyStyle,
       estilo_horizontal = horizontalStyle,
       estilo_total = totalStyle,
-      frecuencias_simples = TRUE, 
+      frecuencias_simples = TRUE,
       tablas_cruzadas = TRUE
       )
-    
+
     # Frecuencias simples
     k1=k1 + 1 + nrow(multiples[[1]][[1]]) + 7
     k2=k2 + 1 + nrow(multiples[[1]][[2]]) + 7
-    
+
     # Tablas cruzadas
     k3=k3 + 1 + nrow(multiples[[2]][[1]]) + 8
     k4=k4 + 1 + nrow(multiples[[2]][[2]]) + 8
-    
+
     np=np + 1
-    
+
   }
   else if (p %in% Lista_Cont){
 
     print(paste('Estimación de pregunta continua:', p))
-    
+
    continuas <-  preguntas(
-     pregunta = p, 
-     num_pregunta = np, 
-     datos=dataset, 
-     DB_Mult = DB_Mult, 
-     dominios = Dominios, 
-     lista_preguntas=Lista_Preg, 
+     pregunta = p,
+     num_pregunta = np,
+     datos=dataset,
+     DB_Mult = DB_Mult,
+     dominios = Dominios,
+     lista_preguntas=Lista_Preg,
      diseño = disenio_cont,
-     wb = wb, 
-     renglon_fs = c(k1, k2), 
+     wb = wb,
+     renglon_fs = c(k1, k2),
      renglon_tc = c(k3, k4),
-     columna = 1, 
+     columna = 1,
      hojas_fs = c(1,3),
-     hojas_tc = c(2,4), 
+     hojas_tc = c(2,4),
      fuente = nombre_proyecto,
      tipo_pregunta = 'continua',
      organismo_participacion = organismo,
@@ -208,38 +225,38 @@ for (p in Lista[133:151]) {
      estilo_categorias = bodyStyle,
      estilo_horizontal = horizontalStyle,
      estilo_total = totalStyle,
-     frecuencias_simples = TRUE, 
+     frecuencias_simples = TRUE,
      tablas_cruzadas = TRUE
      )
-   
+
    # Frecuencias simples
    k1=k1 + 1 + nrow(continuas[[1]][[1]]) + 7
    k2=k2 + 1 + nrow(continuas[[1]][[2]]) + 7
-   
+
    # Tablas cruzadas
    k3=k3 + 1 + nrow(continuas[[2]][[1]]) + 7
    k4=k4 + 1 + nrow(continuas[[2]][[2]]) + 7
-   
+
    np=np + 1
 
   }
   else({
-    
+
     print(paste('Estimación de pregunta categórica:', p))
-  
+
     categoricas <- preguntas(
-      pregunta = p, 
-      num_pregunta = np, 
+      pregunta = p,
+      num_pregunta = np,
       datos = dataset,
-      DB_Mult = DB_Mult, 
+      DB_Mult = DB_Mult,
       dominios = Dominios,
-      lista_preguntas = Lista_Preg, 
-      diseño = disenio_cat, 
-      wb = wb, 
-      renglon_fs = c(k1, k2), 
+      lista_preguntas = Lista_Preg,
+      diseño = disenio_cat,
+      wb = wb,
+      renglon_fs = c(k1, k2),
       renglon_tc = c(k3, k4),
-      columna = 1, 
-      hojas_fs = c(1, 3), 
+      columna = 1,
+      hojas_fs = c(1, 3),
       hojas_tc = c(2, 4),
       fuente = nombre_proyecto,
       tipo_pregunta = 'categorica',
@@ -248,20 +265,20 @@ for (p in Lista[133:151]) {
       estilo_categorias = bodyStyle,
       estilo_horizontal = horizontalStyle,
       estilo_total = totalStyle,
-      frecuencias_simples = TRUE, 
+      frecuencias_simples = TRUE,
       tablas_cruzadas = TRUE)
-    
+
     # Frecuencias simples
     k1=k1 + 1 + nrow(categoricas[[1]][[1]]) + 7
     k2=k2 + 1 + nrow(categoricas[[1]][[2]]) + 7
-    
+
     # Tablas cruzadas
     k3=k3 + 1 + nrow(categoricas[[2]][[1]]) + 8
     k4=k4 + 1 + nrow(categoricas[[2]][[2]]) + 8
-    
+
     np=np + 1
   })
-  
+
 }
 
 openxlsx::openXL(wb)
