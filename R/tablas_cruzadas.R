322027cb7124970bd194f83b6d5e03d971ab6c16
AnaTorresR <anaetorresr@ciencias.unam.mx>
2021-11-04 12:49:02 -0600

    Añadiendo llaves a diseño

--- R/tablas_cruzadas.R
+++ R/tablas_cruzadas.R
@@ -188,7 +188,7 @@ tablas_cruzadas <- function(diseño, pregunta, dominio, datos, DB_Mult,
     for (i in menciones_vector){
       variable <- menciones_juntas %>%
         pull(!!sym(i)) %>%
-      diseño %<>% srvyr::mutate(!!sym(i) := if_else(is.na(aux_missing), aux_missing, variable))
+      {{diseño}} %<>% srvyr::mutate(!!sym(i) := if_else(is.na(aux_missing), aux_missing, variable))
     }
 
     tabla_cruzada = tibble()
